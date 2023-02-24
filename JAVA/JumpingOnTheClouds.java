6ffaa1a21c4f03ab25fd752f06d13b536e263de9
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-02-24 21:24:49 +0530

    Create JumpingOnTheClouds.java

---
+++ JAVA/JumpingOnTheClouds.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr= new int[n+1];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        arr[n] = 1;
+        int jmp=0;
+        for(int i=0; i<n-1;){
+            if(arr[i+2]==0){
+                ++jmp;
+                i+=2;
+            }
+            else{
+                ++jmp;
+                ++i;
+            }
+        }
+        System.out.println(jmp);
+    }
+}
