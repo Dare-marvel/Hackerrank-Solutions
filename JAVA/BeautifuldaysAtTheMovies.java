867e83eedfc55c2e04b77ba50f4a1246cfb1397a
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-02-24 21:22:10 +0530

    Create BeautifuldaysAtTheMovies.java

---
+++ JAVA/BeautifuldaysAtTheMovies.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Reverse{
+    int rev(int num){
+        int rn=0;
+        while(num>0) {
+            rn = 10*rn + num % 10;
+            num=num/10;
+        }
+        return rn;
+    }
+}
+public class BeautifuldaysAtTheMovies{
+    public static void main(String[] args) {
+        int n1,n2,count=0;
+        Reverse r= new Reverse();
+        Scanner sc= new Scanner(System.in);
+        //System.out.println("Starting day:");
+        n1=sc.nextInt();
+      // System.out.println("Ending day:");
+        n2=sc.nextInt();
+       //System.out.println("Divisor");
+       float k=sc.nextInt();
+        for(int i=n1;i<=n2;i++){
+          //  System.out.println("Reverse of "+i+" is "+r.rev(i));
+            float c=(Math.abs(i-r.rev(i)))/k;
+            if(c==Math.round(c)){
+                count++;
+            }
+        }
+        //System.out.println("The number of beautiful days are:");
+        System.out.println(count);
+    }
+}
