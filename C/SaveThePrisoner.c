36630f730231b31cac4c872315966eb3b7e51a1c
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-02-25 14:07:11 +0530

    Update SaveThePrisoner.c

--- C/SaveThePrisoner.c
+++ C/SaveThePrisoner.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+// Link to the problem : https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+#include <stdio.h>
 
 int main()
 {
     int t;
-//printf("Enter the number of testcases:\n");
-scanf("%d",&t);
-while(t--){
-    long p,s,st;
-//printf("Enter the number of prisoners:\n");
-scanf("%ld",&p);
-//printf("Enter the number of sweets:\n");
-scanf("%ld",&s);
-//printf("Enter the number of prisoners:\n");
-scanf("%ld",&st);
+    // printf("Enter the number of testcases:\n");
+    scanf("%d", &t);
+    while (t--)
+    {
+        long p, s, st;
+        // printf("Enter the number of prisoners:\n");
+        scanf("%ld", &p);
+        // printf("Enter the number of sweets:\n");
+        scanf("%ld", &s);
+        // printf("Enter the number of prisoners:\n");
+        scanf("%ld", &st);
 
-printf("%d\n",(s+st-2)%p+1);
+        printf("%d\n", (s + st - 2) % p + 1);
 
-return 0;
-}
+        return 0;
+    }
 }
