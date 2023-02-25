02e9b6fbb618153eee383c9a40e6072e8f263148
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-02-25 13:53:31 +0530

    Create BillDivision.c

---
+++ C/BillDivision.c
@@ -0,0 +1,33 @@
+// Link to problem : https://www.hackerrank.com/challenges/bon-appetit/problem
+
+#include <stdio.h>
+
+int main()
+{
+    int n, k, i, j, ba1 = 0, ba2, bc;
+    // printf("Enter the number of items:");
+    scanf("%d", &n);
+    int bill[n];
+    // printf("Enter the item that anna did'nt eat");
+    scanf("%d", &k);
+    // printf("Enter the prices of respective items:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &bill[i]);
+    }
+    // printf("Enter the bill charged:");
+    scanf("%d", &bc);
+    for (j = 0; j < n; j++)
+    {
+        if (j != k)
+        {
+            ba1 += bill[j];
+        }
+    }
+    ba2 = ba1 / 2;
+    if (ba2 == bc)
+        printf("Bon Appetit");
+    else if (bc > ba2)
+        printf("%d", bc - ba2);
+    return 0;
+}
