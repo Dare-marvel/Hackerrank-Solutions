af5de5e937c60be83914875ab19ee37b592b7922
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-03-26 18:26:22 +0530

    Create FlatlandSpaceStations.py

---
+++ PYTHON/FlatlandSpaceStations.py
@@ -0,0 +1,22 @@
+# Link to the problem : hackerrank.com/challenges/flatland-space-stations/problem?isFullScreen=false
+
+# read input values for number of cities and number of space stations
+n, m = map(int, input().split())
+
+# read the positions of the space stations
+c = sorted(list(map(int, input().split())))
+
+# initialize the maximum distance to be the distance from the first city to the first space station
+# or from the last space station to the last city, whichever is greater
+max_d = max(c[0], n-1-c[-1])
+
+# iterate over the sorted list of space stations and calculate the maximum distance to the nearest space station
+for i in range(1, m):
+    # calculate the distance between the current and previous space stations and divide by 2
+    dist = (c[i] - c[i-1]) // 2
+    
+    # update the maximum distance seen so far if the current distance is greater
+    max_d = max(max_d, dist)
+    
+# print the maximum distance to the nearest space station
+print(max_d)
