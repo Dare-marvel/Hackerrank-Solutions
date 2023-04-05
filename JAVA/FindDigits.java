31213d39bd5cec930a31d81aae175d9b67220bbf
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-04-05 11:42:25 +0530

    Update FindDigits.java

--- JAVA/FindDigits.java
+++ JAVA/FindDigits.java
@@ -1,29 +1,33 @@
 // Link to the problem : https://www.hackerrank.com/challenges/find-digits/problem
 
 import java.util.*;
+
 public class FindDigits {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        //System.out.println("Enter the number of testcases");
-        int t=sc.nextInt();
-        while(t>0){
-           // System.out.println("Enter the number:");
-            int n=sc.nextInt();
-            int num=n;
+        Scanner sc = new Scanner(System.in);
+        // Input the number of test cases
+        int t = sc.nextInt();
+        while (t > 0) {
+            // Input the number
+            int n = sc.nextInt();
+            int num = n;
             int div;
-            int count=0;
-            while(num>0){
-                div=num%10;
-                if(div>0) {
+            int count = 0;
+            // Extract digits from the number
+            while (num > 0) {
+                div = num % 10;
+                if (div > 0) {
+                    // Check if the digit is a divisor of the number
                     if (n % div == 0) {
                         count++;
                     }
                 }
-                num/=10;
+                num /= 10;
             }
-            //System.out.println("Output");
+            // Output the result
             System.out.println(count);
             t--;
         }
     }
 }
+
