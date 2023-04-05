8f1ad58abb3182d086d9e1d797ca020a0e2cd64f
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-04-05 11:38:31 +0530

    Update DesignerPdfViewer.java

--- JAVA/DesignerPdfViewer.java
+++ JAVA/DesignerPdfViewer.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class DesignerPdfViewer {
     public static void main(String[] args) {
-        int [] htarr= new int [26];
-        // System.out.println("Enter the heights of letters:");
+        int [] htarr= new int [26]; // Array to store the heights of letters
         Scanner sc= new Scanner(System.in);
-        for (int i=0;i<26;i++)
+        for (int i=0;i<26;i++) {
+            // Input the heights of letters
             htarr[i] = sc.nextInt();
-        char [] carr= new char[10];
+        }
+        char [] carr= new char[10]; // Array to store the characters of the string
         String str;
-        // System.out.println("Enter the string:");
-        str= sc.next();
-        carr= str.toCharArray();
-        int max=0;
+        str= sc.next(); // Input the string
+        carr= str.toCharArray(); // Convert the string to a character array
+        int max=0; // Variable to store the maximum height
         for (int i=0;i<str.length();i++){
-
             if (htarr[carr[i]-97]>max){
+                // Compare the height of the current character with the current maximum height
+                // If it is greater, update the maximum height
                 max=htarr[carr[i]-97];
             }
         }
-    //    System.out.println("The area is "+max*str.length()+"mm^2");
-       System.out.println(max*str.length());
+        System.out.println(max*str.length()); // Print the area as product of maximum height and string length
     }
 }
