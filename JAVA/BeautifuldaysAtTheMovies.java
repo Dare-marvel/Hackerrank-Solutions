74a89fca53f59a4f12712ceeb992d8673aa13aed
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-04-05 11:36:26 +0530

    Update BeautifuldaysAtTheMovies.java

--- JAVA/BeautifuldaysAtTheMovies.java
+++ JAVA/BeautifuldaysAtTheMovies.java
@@ -1,35 +1,39 @@
 // Link to the problem : https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
 
 import java.util.Scanner;
-class Reverse{
-    int rev(int num){
-        int rn=0;
-        while(num>0) {
-            rn = 10*rn + num % 10;
-            num=num/10;
+
+class Reverse {
+    // Method to reverse an integer
+    int rev(int num) {
+        int rn = 0;
+        while (num > 0) {
+            rn = 10 * rn + num % 10;
+            num = num / 10;
         }
         return rn;
     }
 }
-public class BeautifuldaysAtTheMovies{
+
+public class BeautifuldaysAtTheMovies {
     public static void main(String[] args) {
-        int n1,n2,count=0;
-        Reverse r= new Reverse();
-        Scanner sc= new Scanner(System.in);
-        //System.out.println("Starting day:");
-        n1=sc.nextInt();
-      // System.out.println("Ending day:");
-        n2=sc.nextInt();
-       //System.out.println("Divisor");
-       float k=sc.nextInt();
-        for(int i=n1;i<=n2;i++){
-          //  System.out.println("Reverse of "+i+" is "+r.rev(i));
-            float c=(Math.abs(i-r.rev(i)))/k;
-            if(c==Math.round(c)){
+        int n1, n2, count = 0;
+        Reverse r = new Reverse();
+        Scanner sc = new Scanner(System.in);
+        // Input the starting day
+        n1 = sc.nextInt();
+        // Input the ending day
+        n2 = sc.nextInt();
+        // Input the divisor
+        float k = sc.nextInt();
+        for (int i = n1; i <= n2; i++) {
+            // Calculate the absolute difference between i and its reverse
+            float c = (Math.abs(i - r.rev(i))) / k;
+            // Check if the quotient is an integer
+            if (c == Math.round(c)) {
                 count++;
             }
         }
-        //System.out.println("The number of beautiful days are:");
+        // Output the number of beautiful days
         System.out.println(count);
     }
 }
