78328d9a4fdeae4d89fcead235dfd7ce63848b21
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-04-05 14:22:43 +0530

    Update JumpingOnTheClouds.java

--- JAVA/JumpingOnTheClouds.java
+++ JAVA/JumpingOnTheClouds.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class JumpingOnTheClouds{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int [] arr= new int[n+1];
+        int n=sc.nextInt(); // input the number of clouds
+        int [] arr= new int[n+1]; // create an array to hold the clouds
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=sc.nextInt(); // input the clouds
         }
-        arr[n] = 1;
-        int jmp=0;
-        for(int i=0; i<n-1;){
-            if(arr[i+2]==0){
-                ++jmp;
-                i+=2;
+        arr[n] = 1; // set the last cloud to 1 to mark the end
+        int jmp=0; // variable to hold the number of jumps
+        for(int i=0; i<n-1;){ // loop through the clouds
+            if(arr[i+2]==0){ // check if it's possible to jump 2 clouds
+                ++jmp; // increase the number of jumps
+                i+=2; // jump 2 clouds
             }
-            else{
-                ++jmp;
-                ++i;
+            else{ // if not possible to jump 2 clouds
+                ++jmp; // increase the number of jumps
+                ++i; // jump 1 cloud
             }
         }
-        System.out.println(jmp);
+        System.out.println(jmp); // output the number of jumps
     }
 }
+
